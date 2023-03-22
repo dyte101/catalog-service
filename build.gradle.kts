7ca5fb43d7411de269aacc2d6f2fad715256f664
dyte101 <abubakarr.turay@yahoo.com>
2023-03-22 10:03:33 +0000

    Workflow file updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.yaml:snakeyaml:1.33")
+	implementation("org.yaml:snakeyaml:2.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webflux")
 }
